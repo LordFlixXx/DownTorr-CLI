3538864956589b321dc13c97f1f1ed2eb9ccb191
Samuel Teixeira Passamani <159223630+LordFlixXx@users.noreply.github.com>
2025-04-04 22:11:40 -0300

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,3 +1,3 @@
 ﻿from .download import download_and_process_torrent, download_torrent, rename_files
-from .upload import save_subtitles_to_drive, upload_subtitles
+from .upload import save_subtitles_to_drive, upload_subtitles, upload_files
 from .utils import create_magnet_link, read_json_file
